301737ee37ba408a451c51539aaa68e1a2cc6742
dobbyyo <rlaekals9709@gmail.com>
2021-11-30 18:58:43 +0900

    비디오까지 커밋
    강의 비디오업로드까지 커밋!

---
+++ src/models/Video.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true },
+  fileUrl: { type: String, required: true },
+  description: { type: String, required: true, trim: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, trim: true }],
+  meta: {
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
+  },
+});
+// title: String = title: {type:String} 같음
+
+// videoSchema.pre("save", async function () {
+//   this.hashtags = this.hashtags[0]
+//     .split(",")
+//     .map((word) => (word.startsWith("#") ? word : `#${word}`));
+// });
+
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
+const Video = mongoose.model("Video", videoSchema);
+export default Video;
+
+////////////////
